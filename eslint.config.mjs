4683fe80f03d2a397af43fa478a888e6f3ac8e8e
Chagas011 <washington.chagas.9@hotmail.com>
2025-07-02 17:27:43 -0300

    chore: skipping linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,8 +12,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    files: ["src/generated/**", "src/generated/prisma/**"],
     rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
   },
 ];
